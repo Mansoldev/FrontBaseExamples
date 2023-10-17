03c5114ab06bdcf1b57b13f62b2d933fba872b8d
Mario García Alarcón <mariogarciaalarcon@gmail.com>
2023-10-17 14:26:02 +0200

    No colisiona, no solidifica, no borra

--- COMPLETOS/Tetris/main.js
+++ COMPLETOS/Tetris/main.js
@@ -1,2 +1,154 @@
 import './style.css'
 
+// 1. Inicializar el canvas
+const canvas = document.querySelector('canvas')
+const context = canvas.getContext('2d')
+
+const BLOCK_SIZE = 20
+const BOARD_WIDTH = 14
+const BOARD_HEIGHT = 30
+
+canvas.width = BLOCK_SIZE * BOARD_WIDTH
+canvas.height = BLOCK_SIZE * BOARD_HEIGHT
+
+context.scale(BLOCK_SIZE, BLOCK_SIZE)
+
+// 2. Board
+const board = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1]
+]
+
+// 3. Pieza player
+const piece = {
+    position: { x: 5, y: 5 },
+    shape: [
+        [1, 1],
+        [1, 1]
+    ]
+}
+
+// 4. Game loop
+function update() {
+  draw()
+  window.requestAnimationFrame(update)
+}
+
+function draw() {
+  context.fillStyle = '#000'
+  context.fillRect(0, 0, canvas.width, canvas.height)
+
+  board.forEach((row, y) => {
+    row.forEach((value, x) => {
+        if (value === 1) {
+            context.fillStyle = 'yellow'
+            context.fillRect(x, y, 1, 1)
+        }
+    })
+  })
+
+  piece.shape.forEach((row, y) => {
+    row.forEach((value, x) => {
+        if (value) {
+            context.fillStyle = 'red'
+            context.fillRect(x + piece.position.x, y + piece.position.y, 1, 1)
+        }
+    })
+  })
+}
+
+// 5 User controls
+document.addEventListener('keydown', event => {
+    if (event.key == 'ArrowLeft') {
+        piece.position.x--
+        if (checkCollision()) {
+            piece.position.x++
+        }
+    }
+    if (event.key == 'ArrowRight') {
+        piece.position.x++
+        if (checkCollision()) {
+            piece.position.x--
+        }
+    }
+    if (event.key == 'ArrowDown') {
+        piece.position.y++
+        if (checkCollision()) {
+            piece.position.y--
+            solidifyPiece()
+            removeRows()
+        }
+    }
+})
+
+// 6. Game events
+function checkCollision() {
+    return piece.shape.find((row, y) => {
+        row.find((value, x) => {
+            return (
+                value !== 0 &&
+                board[y + piece.position.y]?.[x + piece.position.x] !== 0
+            )
+        })
+    })
+}
+
+function solidifyPiece() {
+    piece.shape.forEach((row, y) => {
+        row.forEach((value, x) => {
+            if (value === 1) {
+                board[y + piece.position.y][x + piece.position.y] = 1
+            }
+        })
+    })
+
+    piece.position.x = 0
+    piece.position.y = 0
+}
+
+function removeRows() {
+    const rowsToRemove = []
+
+    board.forEach((row, y) => {
+        if (row.every(value => value === 1)) {
+            rowsToRemove.push(y)
+        }
+    })
+
+    rowsToRemove.forEach(y => {
+        board.splice(y, 1)
+        const newRow = Array(BOARD_WIDTH).fill(0)
+        board.unshift(newRow)
+    })
+}
+
+// START
+update();
